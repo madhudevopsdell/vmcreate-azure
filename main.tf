390d6828f9f6e08c9af4aaee435395bd77c80d0a
madhudevopsdell <36254372+madhudevopsdell@users.noreply.github.com>
2022-01-30 13:07:00 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,22 +1,19 @@
-# Configure the Azure provider
+# Configure the azurerm provider source and version requirements
 terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 2.65"
+      version = "~2.88.0"
     }
   }
-
-  required_version = ">= 1.1.0"
 }
-
+ 
+# Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
+ 
   subscription_id = "2ebea6a9-bb90-4f6b-9f44-fac61b0114b3"
   tenant_id       = "f6bd0f9c-ec65-4c2e-98d3-2911014543a1"
-}
-
-resource "azurerm_resource_group" "rg" {
-  name     = "myTFResourceGroup"
-  location = "westus2"
+  client_id       = "0dc0e993-14c6-431e-9970-42fda2a84c0d"
+  client_secret   = "sH~4QCD8G8LrFlC.dhP9iz.3o6-41_g4nh"
 }
